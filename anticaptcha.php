cc9508b46563bea0a40385e5c305d481582ee5b1
Андрей Ильинский <me@tati.pro>
2020-05-08 21:48:34 +0300

    disable certificate validation

--- anticaptcha.php
+++ anticaptcha.php
@@ -117,6 +117,8 @@ class Anticaptcha {
 
 
         $ch = curl_init();
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
         curl_setopt($ch,CURLOPT_URL,"{$this->scheme}://{$this->host}/$methodName");
         curl_setopt($ch,CURLOPT_RETURNTRANSFER,1);
         curl_setopt($ch,CURLOPT_ENCODING,"gzip,deflate");
